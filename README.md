597974e644664d5f339c94bae82b46d50b6483f0
Karan Sakhuja <kevin.sakhuja@gmail.com>
2015-03-22 23:38:16 -0400

    -Tweaks - README

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Edit editor
 
 #### Usage
 Takes a large csv file, and applies filters, breaks it down, converts it into JSON.
+
+```bash
+# Use the CSV sample filter as example
+$ node index.js -h # for usage
+
+Options:
+  -h  Show help
+  -i  [required]  [default: "input.txt"]
+  -o  [required]  [default: "output.txt"]
+  -f  [required]  [default: "sample_filter"]
+```
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 var csv = require('csv').parse,
   fs = require('fs'),
-  argv = require('yargs').argv,
-  Filter = require('./lib/filter');
+  Filter = require('./lib/filter'),
+  argv = require('yargs')
+    .help('h')
+    .usage('Usage: $0 -i [input file] -o [output file] -f [filter file]')
+    .demand(['i','o','f'])
+    .default({ i: 'input.txt', o: 'output.txt', f: 'sample_filter' })
+    .argv;
 
 
 var count = 0;
@@ -10,44 +15,44 @@ var output = [];
 var t1 = Date.now();
 
 
-var fileName = __dirname + '/' + (argv.file || 'input.txt');
-var ws = fs.createWriteStream(__dirname + '/' +(argv.out || 'output.txt'));
-var filterCriteria = require('./' + (argv.filter || 'filter_criteria'));
+var fileName = __dirname+'/'+argv.i;
+var ws = fs.createWriteStream(__dirname+'/'+argv.o);
+var filterCriteria = require('./'+argv.f);
 
 
-// Init things, and init the filter criteria function
+// Init things, and the filter criteria
 var filter = new Filter(filterCriteria);
 ws.write('[\n');
+
 
 var parser = csv({
   delimiter: '  ',
   relax: true
 })
-  .on('readable', function() {
+.on('readable', function() {
 
-    var record = parser.read();
-    record = record && record[0];
+  var record = parser.read();
+  record = record && record[0];
 
-    if (record) {
-      console.log(++count);
-      var out = record.split('\t');
+  if (record) {
+    console.log(++count);
+    var out = record.split('\t');
 
-      // Filtering logic
-      out = filter.run(out);
-      if (out && typeof out === 'object') {
-        ws.write(JSON.stringify(out)+',\n');
-      }
+    // Filtering logic
+    out = filter.run(out);
+    if (out && typeof out === 'object') {
+      ws.write(JSON.stringify(out)+',\n');
     }
-  })
-  .on('error', function(err) {
-    console.log(err.message);
-  })
-  .on('finish', function() {
-    ws.write(']');
-    ws.end();
-    console.log('Total Rows: %s', count);
-    console.log('Time Taken rows: %s seconds', (Date.now() - t1)/1000);
-  });
-
+  }
+})
+.on('error', function(err) {
+  console.log(err.message);
+})
+.on('finish', function() {
+  ws.write(']');
+  ws.end();
+  console.log('Total Rows: %s', count);
+  console.log('Time Taken rows: %s seconds', (Date.now() - t1)/1000);
+});
 
 fs.createReadStream(fileName).pipe(parser);
